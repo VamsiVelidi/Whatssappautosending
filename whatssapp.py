06f3e9bf3ec7c79814f5ddd48d578d2f53842d60
Vamsi Ramesh <vamsirameshvelidi@gmail.com>
2019-10-07 07:47:07 +0530

    Add files via upload

---
+++ whatssapp.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+
+driver = webdriver.Chrome('/home/vamsi/Downloads/chromedriver')
+driver.maximize_window()
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+target = '"Arakonam Vignesh"'
+string = 'Dai saama semma loosu da nee'
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located((
+By.XPATH, x_arg)))
+print ("Wait for few seconds")
+group_title.click()
+message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+for i in range (100):
+	message.send_keys(string)
+	sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+	sendbutton.click()
+# driver.close()
